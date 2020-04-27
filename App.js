663f900d32d75acf069f946a9707ab0d378d9688
Miles Higbie <miles.higbie@gmail.com>
2020-04-27 14:48:04 -0400

    stable version

--- App.js
+++ App.js
@@ -6,34 +6,54 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import { View, Text, Image, StyleSheet } from "react-native"; 
+import React, {useState} from 'react';
+import { View, Text, Image, StyleSheet, FlatList, Alert } from "react-native"; 
+import Header from "./components/Header.js"; 
+import ListItem from "./components/ListItem"
+import AddItem from "./components/AddItem.js"
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: 1, text: "Milk" },
+    {id: 2, text: "Eggs" },
+    {id: 3, text: "Choc0late" },
+    {id: 4, text: "Pecan pah-pah-PIE" },
+  ]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => id != item.id)
+    })
+  }
+
+  const addItem = (text) => {
+    if(text.length > 0){
+      setItems(prevItems => {
+        return [...prevItems, {id: Math.floor(Math.random() * 10), text }]
+      })
+    } else {
+      alert("The item can not be blank")
+    }
+  }
+
+
   return (
-    <View style={style.container}>
-      <Text style={style.text}>Happy Miles Day!</Text>
-      <Image style={style.img} source={{uri: "https://images-na.ssl-images-amazon.com/images/I/61hQylG0ysL._SX466_.jpg"}} /> 
+    <View style={styles.container}>
+      <Header title="Passed Down Title"/>
+      <AddItem addItem={addItem}/>
+      <FlatList 
+        data={items} 
+        renderItem={({item}) => <ListItem deleteItem={deleteItem} item={item}/>} 
+      /> 
     </View>
   )
 }
 
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center'
-  },
-  text: {
-    color: "blue", 
-    fontSize: 10
-  },
-  img: {
-    width: 100,
-    height: 100 , 
-    borderRadius: 100 /2,
-    borderWidth: 2
+    flex: 1,
+    paddingTop: 60
   }
 })
 
