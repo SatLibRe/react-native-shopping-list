29a5fbd45a72dc696622003464cb0a275d1bf15c
Miles Higbie <miles.higbie@gmail.com>
2020-04-27 13:06:52 -0400

    image style

--- App.js
+++ App.js
@@ -12,7 +12,8 @@ import { View, Text, Image, StyleSheet } from "react-native";
 const App = () => {
   return (
     <View style={style.container}>
-      <Text style={style.text}>Happy Birthday!</Text>
+      <Text style={style.text}>Happy Miles Day!</Text>
+      <Image style={style.img} source={{uri: "https://images-na.ssl-images-amazon.com/images/I/61hQylG0ysL._SX466_.jpg"}} /> 
     </View>
   )
 }
@@ -27,6 +28,12 @@ const style = StyleSheet.create({
   text: {
     color: "blue", 
     fontSize: 10
+  },
+  img: {
+    width: 100,
+    height: 100 , 
+    borderRadius: 100 /2,
+    borderWidth: 2
   }
 })
 
